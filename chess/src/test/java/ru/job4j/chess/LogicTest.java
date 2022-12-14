98eebaf3a754a7b9936964d1e11174ec9e995bce
DimPev <dmitriiPev777@gmail.com>
2022-12-14 22:03:11 +0300

    Каркас шахматной доски

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,11 +4,18 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import org.junit.Assert;
+import ru.job4j.chess.firuges.black.PawnBlack;
 
+import java.security.PublicKey;
+import java.util.Arrays;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
+//@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
@@ -19,5 +26,55 @@ public class LogicTest {
             logic.move(Cell.C1, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveThenOccupiedCellException()
+            throws OccupiedCellException {
+        Logic logic = new Logic();
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell startPosition = bishopBlack.position();
+        PawnBlack pawnBlack = new PawnBlack(Cell.D7);
+        logic.add(bishopBlack);
+        logic.add(pawnBlack);
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(startPosition, Cell.E6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("That place locked  another figure");
+    }
+
+    @Test
+    public void whenMoveThenImpossibleMoveException()
+            throws ImpossibleMoveException {
+        Logic logic = new Logic();
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell startPosition = bishopBlack.position();
+        logic.add(bishopBlack);
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(startPosition, Cell.A8);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Could not move by diagonal from C8 to A8");
+    }
+
+    @Test
+    public void whenPositionIsRightBishopBlack() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell startPosition = bishopBlack.position();
+        assertEquals(startPosition, Cell.C8);
+    }
+
+    @Test
+    public void whenCopyPositionIsRightBishopBlack() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        Cell startPosition = bishopBlack.position();
+        assertEquals(startPosition, bishopBlack.copy(startPosition).position());
+    }
+
+    @Test
+    public void whenWayIsCorrect() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Cell[] way = bishopBlack.way(Cell.G5);
+        Cell[] steps = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Assert.assertArrayEquals(way, steps);
     }
 }
